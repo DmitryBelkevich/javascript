d982925d001f6044a9a07822508d32d511c9f8b8
DmitryBelkevich <harleydav@mail.ru>
2026-08-30 07:28:40 +0300

    Update script.js

--- design-patterns/design-patterns/fundamental-patterns/behavioral/state/script.js
+++ design-patterns/design-patterns/fundamental-patterns/behavioral/state/script.js
@@ -1,1 +1,76 @@
+// State
+function State() {
+  this.someMethod = function() {};
+  this.nextState = function() {};
+}
 
+// реализация State
+
+// первое состояние
+function StateA(widjet) {
+  var dublicate = this; // ссылка на инстанцирующийся объект (т.к. this может меняться)
+  
+  this.someMethod = function() {
+    alert("StateA.someMethod");
+    dublicate.nextState();
+  };
+  
+  this.nextState = function() {
+    alert("StateA > StateB");
+    widjet.onNextState(new StateB(widjet));
+  };
+}
+
+StateA.prototype = new State();
+StateA.prototype.constructor = StateA;
+
+// второе состояние
+function StateB(widjet) {
+  var dublicate = this;
+  
+  this.someMethod = function() {
+    alert("StateB.someMethod");
+    dublicate.nextState();
+  };
+  
+  this.nextState = function() {
+    alert("StateB > StateA");
+    widjet.onNextState(new StateA(widjet));
+  };
+}
+
+StateB.prototype = new State();
+StateB.prototype.constructor = StateB;
+
+// "интерфейс" Widget
+
+function Widget() {
+  this.someMethod = function() {};
+  this.onNextState = function(state) {};
+}
+
+// реализация Widget
+
+function Widget1() {
+  var state = new StateA(this);
+  
+  this.someMethod = function() {
+    state.someMethod();
+  };
+  
+  this.onNextState = function(newState) {
+    state = newState;
+  };
+}
+
+Widget1.prototype = new Widget();
+Widget1.prototype.constructor = Widget1;
+
+// использование
+
+var widget = new Widget1();
+
+widget.someMethod(); // StateA.someMethod
+// StateA > StateB
+widget.someMethod(); // StateB.someMethod
+// StateB > StateA
