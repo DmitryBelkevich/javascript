8860ab2c9ad699a2fd630acfbd74cb3222c3de0e
DmitryBelkevich <harleydav@mail.ru>
2026-09-12 18:26:00 +0300

    Update Controller.js

--- design-patterns/mvc/js/mvc/Controller.js
+++ design-patterns/mvc/js/mvc/Controller.js
@@ -3,7 +3,25 @@ import View from './View.js';
 
 export default class Controller {
   constructor() {
+    // model
     const model = new Model();
+
+    // view
     const view = new View();
+
+    // *** binding controller-view ***
+
+    this.view.bindButton1(this.handler1);
+    this.view.bindButton2(this.handler2);
+  }
+
+  // *** handlers ***
+
+  handler1 = () => {
+    console.log("-");
+  }
+
+  handler2 = () => {
+    console.log("+");
   }
 }
