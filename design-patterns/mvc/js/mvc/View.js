0f8f8cf5c431f22c1b08346ded693c1412a526e8
DmitryBelkevich <harleydav@mail.ru>
2026-09-12 17:51:30 +0300

    Update View.js

--- design-patterns/mvc/js/mvc/View.js
+++ design-patterns/mvc/js/mvc/View.js
@@ -2,9 +2,9 @@ export default class View {
   constructor() {
     this.container = document.getElementById('container');
 
-    this.value = document.createElement('div');
+    this.value_e = document.createElement('div');
 
     // fill container
-    this.container.append(this.value);
+    this.container.append(this.value_e);
   }
 }
