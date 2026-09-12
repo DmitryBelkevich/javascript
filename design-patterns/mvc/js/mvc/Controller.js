8c4eb91685b67050a3074274d9bd03b3e50287b4
DmitryBelkevich <harleydav@mail.ru>
2026-09-12 17:36:22 +0300

    Update Controller.js

--- design-patterns/mvc/js/mvc/Controller.js
+++ design-patterns/mvc/js/mvc/Controller.js
@@ -1,3 +1,9 @@
+import Model from './Model.js';
+import View from './View.js';
+
 export default class Controller {
-  
+  constructor() {
+    const model = new Model();
+    const view = new View();
+  }
 }
